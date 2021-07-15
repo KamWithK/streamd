2b788da3bab8f118a3710906da71642a296e56d9
jjia0038 <jjia0038@student.monash.edu>
2021-07-15 10:37:59 +1000

    Created modal for 6 digit code input

--- static/landing.js
+++ static/landing.js
@@ -3,6 +3,17 @@ function aboutClick(){
     window.location.replace("http://127.0.0.1:5500/static/streaming.html");
 }
 
-$("about").click(function(){
-    $("modal").toggleClass("main");
-});
+window.onload = function () {
+    const joinBtn = document.querySelector('#join');
+    const modalBg = document.querySelector('.modal-background');
+    const modal = document.querySelector('.modal');
+
+    joinBtn.addEventListener('click', () => {
+        modal.classList.add('is-active');
+    })
+
+    modalBg.addEventListener('click', () => {
+        modal.classList.remove('is-active');
+    })
+};
+
